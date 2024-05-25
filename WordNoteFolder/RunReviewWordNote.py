eca5a968acc718b3a5c0a6e9a88ba28997344e15
unknown <hk011105@gmail.com>
2024-05-25 16:41:17 +0900

    feat: 쓸데없는 부분 삭제

--- WordNoteFolder/RunReviewWordNote.py
+++ WordNoteFolder/RunReviewWordNote.py
@@ -2,7 +2,4 @@ from ReviewWordNote import ReviewWordNote
 from uitest.WordNoteUI import MainWindow
 
 if __name__ == "__main__": # UI 실행 코드
-    received_word_list = []
-    for i in range(50) :
-        received_word_list.append(i+1)
     reviewWordNote = ReviewWordNote(2,1)
