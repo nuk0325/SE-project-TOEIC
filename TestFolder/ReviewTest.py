d2ea25bbcf0f1f3dadcc63b7c5e84173f58a6d08
hk011105 <hk011105@gmail.com>
2024-05-27 20:05:46 +0900

    feat: 테스트 UI 정상 작동

--- TestFolder/ReviewTest.py
+++ TestFolder/ReviewTest.py
@@ -10,18 +10,16 @@ class ReviewTest(Test) :
         return "복습 테스트"
     
 
-    def _calculateUnit(self) :
+    def _calculateUnit(self) : # 인덱스를 근거로 Part와 Unit을 구하는 함수
         num = self._wordIdxList[0]
         part = num // 150 + 1
         unit = (num % 150) // 10 + 1
         return part, unit
-
     
-    def getUnitNum(self) :
+    def getUnitNum(self) : # UI에 unit1 이런거 값 주는 함수
         unit = self._calculateUnit()[1]
         unitName = "unit" + str(unit)
         return unitName
-
     
     def use_goBack(self) :
         part, unit = self._calculateUnit()
@@ -29,5 +27,4 @@ class ReviewTest(Test) :
 
     def use_gotoSelectTestResult(self) :
         self._dbClose()
-        print("testResult로 이동함")
         Goto.gotoReviewTestResult(self.user, self._correctIdxList, self._wrongIdxList)
