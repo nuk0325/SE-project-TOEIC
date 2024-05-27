ce1622a96a61dc1ae85d7f2f79334fedf8e2de2c
TaehyunLee <taehyun03@gmail.com>
2024-05-27 20:51:37 +0900

    add

--- WordNoteFolder/RunReviewWordNote.py
+++ WordNoteFolder/RunReviewWordNote.py
@@ -1,7 +1,7 @@
 from ReviewWordNote import ReviewWordNote
 from PyQt6.QtWidgets import QApplication
 import sys
-##
+
 if __name__ == "__main__": # UI 실행 코드
     app = QApplication(sys.argv)
     ReviewWordNote("justID", 2,1)
