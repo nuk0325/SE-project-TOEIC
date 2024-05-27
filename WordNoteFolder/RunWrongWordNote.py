c9289b733a5b82ff14e229f4d3e01349986171d2
TaehyunLee <taehyun03@gmail.com>
2024-05-27 19:55:47 +0900

    add :##

--- WordNoteFolder/RunWrongWordNote.py
+++ WordNoteFolder/RunWrongWordNote.py
@@ -1,7 +1,7 @@
 from WrongWordNote import WrongWordNote
 from PyQt6.QtWidgets import QApplication
 import sys
-
+##
 if __name__ == "__main__": # UI 실행 코드
     app = QApplication(sys.argv)
     wrongWordNote = WrongWordNote()
