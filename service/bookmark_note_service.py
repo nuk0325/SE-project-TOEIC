11068a50aed5d25887953c9a710c96f10908dba2
sunwook <117016927+nuk0325@users.noreply.github.com>
2024-05-31 13:23:35 +0900

    feat: 오류 수정

--- service/bookmark_note_service.py
+++ service/bookmark_note_service.py
@@ -6,7 +6,11 @@ class BookmarkNote(WordNote) :
     def __init__(self, user) :
         self.user = user
         self._titleName = "즐겨찾기"
+<<<<<<< Updated upstream
         self._label = self._makeLabel() # 뜻 전체 보기 왼쪽에 있는 label
+=======
+        self._label = self._makeLabel()
+>>>>>>> Stashed changes
         self._testName = "즐겨찾기 테스트 시작"
         self._testChoice = False
         self.db = self._makeDBobj()
