91968c6cf78b209aef08fd5290cb483456aef01f
sunwook <117016927+nuk0325@users.noreply.github.com>
2024-05-31 23:05:24 +0900

    feat: 전체테스트 UI 수정

--- service/UI/prepare_entire_test_ui.py
+++ service/UI/prepare_entire_test_ui.py
@@ -122,7 +122,7 @@ class PrepareEntireTestUI(object):
         self.label.setObjectName("label")
         self.comboBox = QtWidgets.QComboBox(parent=self.frame_3)
         self.comboBox.setGeometry(QtCore.QRect(110, 90, 131, 31))
-        self.comboBox.setStyleSheet("color: black;\n"
+        self.comboBox.setStyleSheet("color: white;\n"
 "background: transparent;\n"
 "border: 1px solid rgb(190, 190, 190);\n"
 "border-radius: 8px;")
